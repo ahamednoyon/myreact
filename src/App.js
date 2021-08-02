c33c756bec11d6a80b768de239695cd34403631e
Ahamed Noyone <ahamednoyon1997@gmail.comm>
2021-08-02 21:50:34 +0600

    all files added and first time react use

--- src/App.js
+++ src/App.js
@@ -6,20 +6,40 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 style={{color:'cyan'}}>My first React App</h2>
+        <Person name='Hero Alam' job='Singer'></Person>
+        <Person name='Salman Sha'job='Actor'></Person>
+        <Person name='Manna De' job='Hero'></Person>
+        <Person name='Rahim Ali' job='Baker'></Person>
+        
+        
+        
+        
       </header>
     </div>
-  );
+  );///////
+}
+
+
+function Person(props){
+  const personStyle = {
+    color:'white',
+    background: 'skyblue', 
+    padding: '5px',
+    margin: '10px 0px',
+    border: '2px solid red',
+    borderRadius: '5px',
+    width: '400px',
+    height: '200px',
+  }
+  return(
+    <div style={personStyle}>
+
+    <h3>Name : {props.name}</h3>
+    <h5>Profession : {props.job}</h5>
+
+    </div>
+  )
 }
 
 export default App;
