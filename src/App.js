43e0a4ae1e86eaf483d2db4e913db663d8e0714e
Ahamed Noyone <ahamednoyon1997@gmail.comm>
2021-08-08 16:38:40 +0600

    load dynamic data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 
 function App() {
-  // const friendsName =['Rahim', 'Karim', 'Jarim', 'Lorim', 'Rahim2', 'Karim2', 'Jarim2', 'Lorim2'];
-  // console.log(friendsName);
+  const [users, setUsers] = useState([])
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+
+  }, [])
 
   const products = [
     {name: 'Photoshop', price: '$190.90'},
@@ -28,7 +33,7 @@ function App() {
         <main className="box">
         
         {
-          products.map(pd => <Product name={pd.name} price={pd.price}></Product>)
+          products.map(pd => <Product productName={pd.name}  productPrice={pd.price}></Product>)
         }
        
         <MovieCounter></MovieCounter>
@@ -36,7 +41,15 @@ function App() {
         <MovieCounter></MovieCounter>
         <MovieCounter></MovieCounter>
         <MovieCounter></MovieCounter>
+        <MovieCounter></MovieCounter>
+
+        {
+          users.map(us => <UsersList name={us.name} key={us.id} email={us.email} address={us.address.city}></UsersList>)
+        }
         </main>
+        
+
+        
         
 
       </div>
@@ -44,35 +57,43 @@ function App() {
   );
 }
 
-function MovieCounter(){
-  const [count, setCount] = useState(5);
-  const handleClick = () => setCount(count + 1);
-
-  return(
-
-    <div className="movie-style">
-
-      <h2>The number of movie {count}</h2>
-      <button onClick={handleClick}>Add Movie</button>
-      
-    </div>
-
-  )
-}
-
-
 
 function Product(props){
   return(
     <div className="product-style">
-      <h3>{props.name}</h3>
-      <h2>{props.price}</h2>
+      <h3>{props.productName}</h3>
+      <h2>{props.productPrice}</h2>
       <button className="btn-style">Buy Now</button>
     </div>
   )
 
 }
 
+
+function MovieCounter(){
+  const [count, setCount] = useState(5);
+  const handleClick = () => setCount(count + 1);
+
+  return(
+    <div className="movie-style">
+      <h2>The number of movie {count}</h2>
+      <button onClick={handleClick}>Add Movie</button>
+    </div>
+  )
+}
+
+
+
+
+function UsersList(props){
+  return(
+    <div className="user-style">
+      <h3>Name: {props.name} </h3>
+      <h4>Email: {props.email} </h4>
+      <h5>Address: {props.address}</h5>
+    </div>
+  )
+}
 
 
 
